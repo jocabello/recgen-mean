b414d1dfc243a12e5b3c71a9f2445ff3e3fc4e0d
José Cabello <jose.icc87@gmail.com>
2020-09-30 23:46:51 -0300

    arreglada ruta para desplegar front en angular

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const indexRouter = require('./routes/index')
 const mongoose = require('mongoose')
+const path = require('path')
 
 app.use('/', indexRouter)
 
@@ -14,10 +15,12 @@ const db = mongoose.connection
 db.on('error', error => console.log(error))
 db.once('open', () => console.log('DB connected'))
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server running on port', process.env.PORT)
 })
-
-
-
-//debo pasar dotenv a -D?
